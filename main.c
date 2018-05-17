1da196c41a76934fa9f3119a8f4ff3f1ad0985b9
Henry <38134284+akahenry@users.noreply.github.com>
2018-05-17 13:16:56 -0300

    Now, the life's bar is decreasing per time
    I've created two fill's bar which are at the life's bar position, they will decrease when pass a certain time, making that life's bar could decrease.

--- main.c
+++ main.c
@@ -8,9 +8,13 @@ sfSprite* sfSprite_createFromFile(const char* filename);
 
 int main()
 {
-    //Declaring some variables
+    //Video variables
     sfVideoMode mode = {800, 600, 32};
     sfRenderWindow* window;
+    sfEvent event;
+
+
+    //Sprite variables
     sfSprite* ship;
     sfSprite* fire;
     sfSprite* background;
@@ -18,37 +22,53 @@ int main()
     sfSprite* life2;
     sfSprite* life3;
     sfSprite* lifebar;
-    //Time's variables
+
+
+    //Time variables
     sfClock* clock = sfClock_create();
     sfTime time = sfClock_getElapsedTime(clock);
     sfTime lasttime = time;
     float dtime;
 
-    sfRectangleShape* base = sfRectangleShape_create();
-    sfRectangleShape_setSize(base, (sfVector2f){800, 100});
-    sfRectangleShape_setPosition(base, (sfVector2f){0, 500});
-    sfRectangleShape_setFillColor(base, sfColor_fromRGB(150,0,0));
 
-    sfEvent event;
-    int mouseX, mouseY;
+    //Flags
     int isFireable = 1; //Fire flag
+
+
+    //Mouse variables
+    int mouseX, mouseY;
+
+
+    //Float variables
+    float sizex = 436;
+    float sizey = 35;
+
+
 
     //Create the main window
     window = sfRenderWindow_create(mode, "Megamania", sfResize | sfClose, NULL);
+
+
 
     ///Loads some sprites
     //Ship
     ship = sfSprite_createFromFile("nave.png");
     sfSprite_scale(ship, (sfVector2f){0.8,0.8}); //nave.png width: 80px
     sfSprite_setPosition(ship, (sfVector2f){450, 450});
+
+
     //Fire
     fire = sfSprite_createFromFile("fire.png");
     sfSprite_scale(fire, (sfVector2f){0.5,0.5});
     sfSprite_setPosition(fire, (sfVector2f){-40, -40});
+
+
     //Background
     background = sfSprite_createFromFile("background.png");
     sfSprite_scale(background, (sfVector2f){3.125, 2.3475});
     sfSprite_setPosition(background, (sfVector2f){400, 300});
+
+
     //Lifes
     life1 = sfSprite_createFromFile("life.png");
     sfSprite_setPosition(life1, (sfVector2f){350, 575});
@@ -56,11 +76,33 @@ int main()
     sfSprite_setPosition(life2, (sfVector2f){400, 575});
     life3 = sfSprite_createFromFile("life.png");
     sfSprite_setPosition(life3, (sfVector2f){450, 575});
+
+
     //Life bar
     lifebar = sfSprite_createFromFile("lifebar.png");
     sfSprite_setOrigin(lifebar, (sfVector2f){111, 19.99});
     sfSprite_scale(lifebar, (sfVector2f){2, 1});
     sfSprite_setPosition(lifebar, (sfVector2f){400, 525});
+
+    sfRectangleShape* fillLifeBar = sfRectangleShape_create();
+    sfRectangleShape_setSize(fillLifeBar, (sfVector2f){436, 35});
+    sfRectangleShape_setOrigin(fillLifeBar, (sfVector2f){218, 17.5});
+    sfRectangleShape_setPosition(fillLifeBar, (sfVector2f){400, 525});
+    sfRectangleShape_setFillColor(fillLifeBar, sfColor_fromRGB(255,255,255));
+
+    sfRectangleShape* fillLifeBar2 = sfRectangleShape_create();
+    sfRectangleShape_setSize(fillLifeBar2, (sfVector2f){sizex, sizey});
+    sfRectangleShape_setPosition(fillLifeBar2, (sfVector2f){182, 507.5});
+    sfRectangleShape_setFillColor(fillLifeBar2, sfColor_fromRGB(100,0,0));
+
+
+    //GUI
+    sfRectangleShape* base = sfRectangleShape_create();
+    sfRectangleShape_setSize(base, (sfVector2f){800, 100});
+    sfRectangleShape_setPosition(base, (sfVector2f){0, 500});
+    sfRectangleShape_setFillColor(base, sfColor_fromRGB(150,0,0));
+
+
 
     ///Start the game loop
     while (sfRenderWindow_isOpen(window))
@@ -97,6 +139,14 @@ int main()
         else
             isFireable = 1;
 
+        if(sizex > 0)
+        {
+            sizex -= 10*dtime;
+            sfRectangleShape_setSize(fillLifeBar2, (sfVector2f){sizex, sizey});
+        }
+
+
+
         ///Actual drawing
         sfRenderWindow_clear(window, sfColor_fromRGB(0,0,0));
         sfRenderWindow_drawSprite(window, background, NULL);
@@ -107,6 +157,8 @@ int main()
         sfRenderWindow_drawSprite(window, life2, NULL);
         sfRenderWindow_drawSprite(window, life3, NULL);
         sfRenderWindow_drawSprite(window, lifebar, NULL);
+        sfRenderWindow_drawRectangleShape(window, fillLifeBar, NULL);
+        sfRenderWindow_drawRectangleShape(window, fillLifeBar2, NULL);
         sfRenderWindow_display(window);
     }
 
@@ -134,4 +186,5 @@ sfSprite* sfSprite_createFromFile(const char* filename)
 
     return sprite;
 }
+
 
